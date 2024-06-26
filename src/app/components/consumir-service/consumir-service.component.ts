f30026716d0deb87c516c895f71d6ffab6558d6f
aaglis <aglissilva17@gmail.com>
2024-06-26 14:38:06 -0300

    feat: service: formas de resgatar dados

--- src/app/components/consumir-service/consumir-service.component.ts
+++ src/app/components/consumir-service/consumir-service.component.ts
@@ -1,29 +1,55 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-consumir-service',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './consumir-service.component.html',
   styleUrl: './consumir-service.component.scss'
 })
-export class ConsumirServiceComponent implements OnInit{
-  //-> Injeção de Dependência <-
-  //forma antiga:
-  // constructor(private apiService$: ApiService) {}
+export class ConsumirServiceComponent {
+  /*
+  INJEÇÃO DE DEPENDÊNCIA
+  forma antiga:
+  constructor(private apiService$: ApiService) {}
+  */
   //forma nova:
   private apiService = inject(ApiService)
-   listItems: Array<{id: string, title: string}> = []
 
-  ngOnInit(): void {
-    this.apiService.httpListItems$().subscribe({
-      next: (next) => {
-        this.listItems = next
-      },
-      error: (error) => console.log(error),
-      complete: () => console.log('Observable listItems$ foi completado')
-    })
-  }
+
+  //RESGATANDO DADOS DE UMA API
+
+  /*
+    FORMA 1:
+    insiro em uma variavel o observable, deixando o próprio angular
+    resgatar os dados e inserir eles no html usando o | async
+  */
+ listItems$ = this.apiService.httpListItems$().pipe(take(1))
+
+
+
+ /*
+    FORMA 2:
+    crio uma variavel do tipo de acordo com api
+    (no caso, um array de objetos), faço o resgate
+    dos dados em um hook (OnInit, por exemplo), e
+    insiro os dados na variável
+
+    listItems: Array<{id: string, title: string}> = []
+
+    ngOnInit(): void {
+      this.apiService.httpListItems$().subscribe( data => {
+        console.log(data)
+        this.listItems = data
+      })
+    }
+
+ */
+
+
+
 
 }
